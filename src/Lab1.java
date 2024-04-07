cdb9d25e42b036ce3cc577219980f85f63d7680e
MindokRuvaak <cole_daniel@rocketmail.com>
2024-04-07 12:35:49 +0200

    Insertion sort a-ok, changed while to for

--- src/Lab1.java
+++ src/Lab1.java
@@ -5,16 +5,20 @@ public class Lab1 {
 
     // for testing
     public static void main(String[] args) {
-        int[] ints = {2, 3, 5};
+        int[] ints = { 2, 3, 5 };
         insertionSort(ints);
         System.out.println(Arrays.toString(ints));
-        ints = new int[]{1, 3};
+        ints = new int[] { 1, 3 };
         insertionSort(ints);
         System.out.println(Arrays.toString(ints));
-        ints = new int[]{4, 2};
+        ints = new int[] { 4, 2 };
         insertionSort(ints);
         System.out.println(Arrays.toString(ints));
-        ints = new int[]{2, 4};
+        ints = new int[] { 2, 4 };
+        insertionSort(ints);
+        System.out.println(Arrays.toString(ints));
+
+        ints = new int[] { 5, 3, 9, 2, 8 };
         insertionSort(ints);
         System.out.println(Arrays.toString(ints));
     }
@@ -25,17 +29,14 @@ public class Lab1 {
 
     // Insertion sort.
     public static void insertionSort(int[] array) {
-        int lastSortedIndex = 0;
-        while (lastSortedIndex < array.length - 1) {
-            insert(array, lastSortedIndex, array[lastSortedIndex + 1]);
-            lastSortedIndex++;
+        for (int lastSortedIndex = 0; lastSortedIndex < array.length - 1; lastSortedIndex++) {
+            insert(array, lastSortedIndex, array[lastSortedIndex+1]);
         }
     }
 
     private static void insert(int[] array, int lastSortedIndex, int toBeInserted) {
         for (int i = lastSortedIndex; i >= 0; i--) {
             if (array[i] < toBeInserted) {
-                array[i + 1] = toBeInserted;
                 return;
             }
             swap(array, i, i + 1);
