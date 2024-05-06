58adbb72071be2417fc7941545ee1442e143630e
MindokRuvaak <cole_daniel@rocketmail.com>
2024-05-06 13:03:52 +0200

    added some more functions and used auto-format

--- Lab3/AATree.hs
+++ Lab3/AATree.hs
@@ -2,22 +2,23 @@
 
 --------------------------------------------------------------------------------
 
-module AATree (
-  AATree,        -- type of AA search trees
-  emptyTree,     -- AATree a
-  get,           -- Ord a => a -> AATree a -> Maybe a
-  insert,        -- Ord a => a -> AATree a -> AATree a
-  inorder,       -- AATree a -> [a]
-  remove,        -- Ord a => a -> AATree a -> AATree a
-  size,          -- AATree a -> Int
-  height,        -- AATree a -> Int
-  checkTree      -- Ord a => AATree a -> Bool
- ) where
+module AATree
+  ( AATree, -- type of AA search trees
+    emptyTree, -- AATree a
+    get, -- Ord a => a -> AATree a -> Maybe a
+    insert, -- Ord a => a -> AATree a -> AATree a
+    inorder, -- AATree a -> [a]
+    remove, -- Ord a => a -> AATree a -> AATree a
+    size, -- AATree a -> Int
+    height, -- AATree a -> Int
+    checkTree, -- Ord a => AATree a -> Bool
+  )
+where
 
 --------------------------------------------------------------------------------
 
 -- AA search trees
-data AATree a 
+data AATree a
   = Empty
   | Node Level (AATree a) a (AATree a)
   deriving (Eq, Show, Read)
@@ -26,40 +27,46 @@ emptyTree :: AATree a
 emptyTree = Empty
 
 get :: Ord a => a -> AATree a -> Maybe a
-get _ Empty  = Nothing
+get _ Empty = Nothing
 get toGet (Node _ l v r) = case compare toGet v of
   LT -> get toGet l
   GT -> get toGet r
   EQ -> Just v
 
-
 -- You may find it helpful to define
 
 --
 split :: AATree a -> AATree a
-split x@(Node xk a xv y@(Node yk b yv z)) 
-  = if xk == yk && xk == level z 
-    then let newX = Node xk a xv b in 
-      Node (yk+1) newX yv z
+split x@(Node xk a xv y@(Node yk b yv z)) =
+  if xk == yk && xk == level z
+    then
+      let newX = Node xk a xv b
+       in Node (yk + 1) newX yv z
     else x -- not a 4 node
--- if not a 4 node given, just return tree unchanged
+    -- if not a 4 node given, just return tree unchanged
 split tree = tree
 
-skew  :: AATree a -> AATree a
+skew :: AATree a -> AATree a
 -- if malformed 3-node
-skew y@(Node yk x@(Node xk a xv b) yv c) 
-  = if yk == xk 
-    then let newY = Node yk b yv c in 
-      Node xk a xv newY
+skew y@(Node yk x@(Node xk a xv b) yv c) =
+  if yk == xk
+    then
+      let newY = Node yk b yv c
+       in Node xk a xv newY
     else y
--- a malformed 4-node will be caught by
+-- a malformed 4-node will be fixed by algorithm for malformed 3-node
 skew a = a
 
 -- and call these from insert.
 insert :: Ord a => a -> AATree a -> AATree a
 insert toInsert = split . skew . insert' toInsert
-  where 
-    insert' = undefined
+  where
+    insert' _ Empty = leaf toInsert
+    insert' val (Node k l v r) = case compare val v of
+      LT -> insert val l
+      GT -> insert val r
+      EQ -> Node k l val r
+
 
 inorder :: AATree a -> [a]
 inorder = error "inorder not implemented"
@@ -68,35 +75,41 @@ size :: AATree a -> Int
 size = error "size not implemented"
 
 height :: AATree a -> Int
-height = error "height not implemented"
+height = level
 
 --------------------------------------------------------------------------------
 -- Optional function
 
 remove :: Ord a => a -> AATree a -> AATree a
-remove = error "remove not implemented"
---------------------------------------------------------------------------------
+remove _ Empty = Empty
+remove toRemove t@(Node k l v r) = case compare toRemove v of
+  LT -> Node k (remove toRemove l) v r
+  EQ -> error "remove not implemented"
+  GT -> Node k l v (remove toRemove r)
 
+
+--------------------------------------------------------------------------------
+-- other help functions
 type Level = Int
 
 level :: AATree a -> Level
-level Empty          = 0
+level Empty = 0
 level (Node k _ _ _) = k
 
-singleton :: a -> AATree a
-singleton v = Node 1 Empty v Empty
+leaf :: a -> AATree a
+leaf v = Node 1 Empty v Empty
 
 --------------------------------------------------------------------------------
 -- Check that an AA tree is ordered and obeys the AA invariants
 
 checkTree :: Ord a => AATree a -> Bool
 checkTree root =
-  isSorted (inorder root) &&
-  all checkLevels (nodes root)
+  isSorted (inorder root)
+    && all checkLevels (nodes root)
   where
     nodes x
       | isEmpty x = []
-      | otherwise = x:nodes (leftSub x) ++ nodes (rightSub x)
+      | otherwise = x : nodes (leftSub x) ++ nodes (rightSub x)
 
 -- True if the given list is ordered
 isSorted :: Ord a => [a] -> Bool
@@ -122,4 +135,3 @@ rightSub :: AATree a -> AATree a
 rightSub = error "rightSub not implemented"
 
 --------------------------------------------------------------------------------
-
