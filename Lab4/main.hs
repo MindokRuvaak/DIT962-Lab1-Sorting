3dec66137f9ce7839c1104cca66094cdc6a62bb9
Nikhil Olsson Mukhopadhyay <gusnikhol@gu.student.se>
2024-05-21 17:21:17 +0200

    refactored code to use PSQ instead of homemade shit

--- Lab4/main.hs
+++ Lab4/main.hs
@@ -1,30 +1,18 @@
 import Route
 import RouteGUI
 import Graph  -- Create a module and use a sensible graph representation
-import PrioSkew
-import qualified PrioSkew as PQ -- OBS: a max-heap
+import Data.PSQueue (PSQ)
+import qualified Data.PSQueue as PQ
 import Data.Map (Map)
 import qualified Data.Map as M
 import Data.Maybe (isNothing, fromJust)
 import System.Environment
 
 
--- A djusktra node with a node, its predecessor and the cost to get there
-data Dijk a b
-  = Dijk {
-    to :: a,
-    dist :: b,
-    from :: a} deriving (Eq, Show)
-
-instance (Ord a, Ord b) => Ord (Dijk a b) where
-  compare d1 d2 = compare (dist d2) (dist d1)
--- higher priority on shorter distance
-
 -- use the djukstra function to compute the set of nodes we can reach and the cost to do so 
--- (we call these Djikstra nodes or Djik for short) and then compute the shortest path between
--- two given nodes, returns nothing if there is no path
+-- and then compute the shortest path between two given nodes, returns nothing if there is no path
 shortestPath :: (Ord a, Ord b, Num b) => Graph a b -> a -> a -> Maybe ([a], b)
-shortestPath g from to = findShortest from to (dijkstra g M.empty (PQ.skewLeaf (Dijk from 0 from)))
+shortestPath g from to = findShortest from to (dijkstra g M.empty (PQ.singleton (from , from) 0))
  -- TODO: implement Dijkstra's algorithm
 
 -- find the shortest path between two nodes given a set of Djikstra nodes
@@ -46,25 +34,29 @@ gatherStops stops from to set
       -- use reverse here to reduce constant factors, still O(n)
         in reverse (to : gatherStops stops from prevTo set)
 
-dijkstra :: (Ord a, Ord b, Num b) => Graph a b -> Map a (b, a) -> SkewHeap (Dijk a b) -> Map a (b, a)
+dijkstra :: (Ord a, Ord b, Num b) => Graph a b -> Map a (b, a) -> PSQ (a, a) b -> Map a (b, a)
 dijkstra g s q
   -- if queue is empty return the set of all Djikstra nodes
-  | isNothing $ PQ.rootOf q = s
+  | isNothing $ PQ.findMin q = s
   -- otherwise check if we already have seen the node
   | otherwise = if  x `M.notMember` s
     -- if we have'nt seen the node then we add it to the set of Djikstra nodes
     -- and add all adjacent nodes to the queue (as Djikstra nodes) 
-    then dijkstra g (M.insert x (d, z) s) (foldr (insert . toDijk d) q' (adj x g))
+    then dijkstra g (M.insert x (d, z) s) (foldr (insertQ . toKeyValuePair d) q' (adj x g))
     -- if we have seen the node already then do nothing
     else dijkstra g s q'
     where
       -- dequeue operation
-      (Dijk x d z) = fromJust $ PQ.rootOf q
-      q' = PQ.removeRoot q
+      ((x , z) PQ.:->  d) = fromJust $ PQ.findMin q
+      q' = PQ.deleteMin q
 
 -- help function to convert the output of adj to Djikstra nodes
-toDijk :: (Num b) => b -> Edge a b -> Dijk a b
-toDijk d e = Dijk (dst e) (d + label e) (src e)
+toKeyValuePair :: (Num b) => b -> Edge a b -> ((a,a), b)
+toKeyValuePair d e = ((dst e, src e) , d + label e)
+
+-- insert our touple of values into a PSQ correctly
+insertQ :: (Ord a, Ord b) => ((a,a), b) -> PSQ (a,a) b -> PSQ (a,a) b
+insertQ (value, prio) = PQ.insert value prio
 
 graphBuilder :: [Stop] -> [LineStop] -> Graph String Integer
 graphBuilder stops lineStops = graphBuilder' lineStops $ foldr (addVertex . name) Graph.empty stops
