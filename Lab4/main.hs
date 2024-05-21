8a85da152f24eadc099caf2ecef9f5d2b936856e
MindokRuvaak <cole_daniel@rocketmail.com>
2024-05-21 11:33:25 +0200

    mby dijkstra finished?

--- Lab4/main.hs
+++ Lab4/main.hs
@@ -2,22 +2,43 @@ import Route
 import RouteGUI
 import Graph  -- Create a module and use a sensible graph representation
 import PrioSkew
-import qualified PrioSkew as PQ
+import qualified PrioSkew as PQ -- OBS: a max-heap
 import Data.Map (Map)
 import qualified Data.Map as M
+import Data.Maybe (isNothing, fromJust)
 
-data Dijk a b = Dijk 
-  {to :: a,
-  dist :: b,
-  from :: a}
+data Dijk a b
+  = Dijk {
+    to :: a,
+    dist :: b,
+    from :: a} deriving (Eq, Show)
 
-shortestPath :: Graph a b -> a -> a -> Maybe ([a], b)
-shortestPath g from to = convert $ dijkstra g s q -- TODO: implement Dijkstra's algorithm
-  where 
-     s = PQ.empty
-     q = PQ.insert (toDijkstraElem ) PQ.empty
+instance (Ord a, Ord b) => Ord (Dijk a b) where
+  compare d1 d2 = compare (dist d2) (dist d1)
+-- higher priority on shorter distance
 
-dijkstra :: Graph a b -> SkewHeap Dijk -> SkewHeap Dijk -> SkewHeap Dijk
+
+shortestPath :: (Ord a, Ord b, Num b) => Graph a b -> a -> a -> Maybe ([a], b)
+shortestPath g from to = convert $ dijkstra g M.empty (PQ.insert (Dijk from 0 from) PQ.empty)
+ -- TODO: implement Dijkstra's algorithm
+
+convert :: Map a (b,a) -> Maybe ([a],b)
+convert = undefined
+
+dijkstra :: (Ord a, Ord b) => Graph a b -> Map a (b, a) -> SkewHeap (Dijk a b) -> Map a (b, a)
+dijkstra g s q
+  | isNothing $ PQ.rootOf q = s
+  | otherwise = if  x `M.notMember` s
+    then dijkstra g (M.insert x (d, z) s) (foldr (insert . toDijk) q' (adj x g))
+    else dijkstra g s q'
+    where
+      (Dijk x d z) = fromJust $ PQ.rootOf q
+      q' = PQ.removeRoot q
+
+
+toDijk :: Edge a b -> Dijk a b
+toDijk e = Dijk (dst e) (label e) (src e)
+
 
 main :: IO ()
 main = undefined  -- TODO: read arguments, build graph, output shortest path
