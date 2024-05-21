5407353c5708f2d8c5d10a82379b111a7c1913c9
MindokRuvaak <cole_daniel@rocketmail.com>
2024-05-21 10:07:06 +0200

    dsaf

--- Lab4/main.hs
+++ Lab4/main.hs
@@ -1,9 +1,23 @@
 import Route
 import RouteGUI
 import Graph  -- Create a module and use a sensible graph representation
+import PrioSkew
+import qualified PrioSkew as PQ
+import Data.Map (Map)
+import qualified Data.Map as M
+
+data Dijk a b = Dijk 
+  {to :: a,
+  dist :: b,
+  from :: a}
 
 shortestPath :: Graph a b -> a -> a -> Maybe ([a], b)
-shortestPath g from to = undefined -- TODO: implement Dijkstra's algorithm
+shortestPath g from to = convert $ dijkstra g s q -- TODO: implement Dijkstra's algorithm
+  where 
+     s = PQ.empty
+     q = PQ.insert (toDijkstraElem ) PQ.empty
+
+dijkstra :: Graph a b -> SkewHeap Dijk -> SkewHeap Dijk -> SkewHeap Dijk
 
 main :: IO ()
 main = undefined  -- TODO: read arguments, build graph, output shortest path
